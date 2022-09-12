bff4101fa87e6068b812d15862a8e89a886ee431
Yann <metalyanni@gmail.com>
2022-09-12 13:45:01 -0500

    Forma de realizar exportaciones simples multiples y diferentes modos de realizar cada una en JS

--- 02-intro-javascript/src/data/heroes.js
+++ 02-intro-javascript/src/data/heroes.js
@@ -26,3 +26,32 @@ export const heroes = [
         owner: 'Marvel'
     },
 ];
+
+export const owners = ['DC','Marvel'];
+
+//Exportando por defecto:
+export default heroes;
+
+/**
+ * NOTA:
+ * 
+ * Si usamos una exportación por defecto y otra como el arreglo owners
+ * entonces hay que realizar una desestructuración como la siguiente:
+ * 
+ * import heroes, { owners } from '../data/heroes';
+ * 
+ */
+
+
+
+/**
+ * Otra manera de realizar la exportación es de la siguiente manera
+ * 
+ * export{
+    heroes,
+    owners
+   }
+ * 
+ * Y se debería Importar de la siguiente manera:
+ * import { heroes, owners } from '../data/heroes';
+ */
