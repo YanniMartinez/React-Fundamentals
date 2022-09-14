5ad33566ea02a3abe65087c43567c1747a360323
Yann <metalyanni@gmail.com>
2022-09-14 13:32:51 -0500

    Añadiendo Babel a nuestro proyecto para pruebas recordemos que babel nos ayuda a crear una compatibilidad de nuestro codigo actual de JS con código para que sea soportado por diversos navegadores o en su defecto por versiones más antiguas

---
+++ 03-counter-app-vite/test/base-pruebas/02-template-string.test.js
@@ -0,0 +1,12 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe('Pruebas en 02-template-string', () => {
+  test('getSaludo debe retornar "Hola Yanni" ', () => {
+    //1
+    const name='Yanni';
+    //2
+    const message = getSaludo(name);
+    //3
+  })
+  
+})
