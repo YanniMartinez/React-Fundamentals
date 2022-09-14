2ee816d6706c968f665376d8fc56e7af1baed860
Yann <metalyanni@gmail.com>
2022-09-14 12:45:17 -0500

    Primera prueba sin fallas con JEST Para ejecutar nuestras pruebas podemos poner YARN test

--- 03-counter-app-vite/test/demo.test.js
+++ 03-counter-app-vite/test/demo.test.js
@@ -3,5 +3,11 @@
  * test('Titulo de nuestra prueba', callBack() )
 */
 test('Esta prueba no debe de fallar', () => {
-    
+    if( 1 === 0 ){
+        throw new Error('No puede dividir entre cero');
+    }
+    //Esta  prueba fallará por que 0/0 no es posible
+    // if( 0 === 0 ){
+    //     throw new Error('No puede dividir entre cero');
+    // }
 })
