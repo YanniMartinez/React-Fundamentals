ee54529e7ff3df47985b6c30673706c98120d318
Yann <metalyanni@gmail.com>
2022-09-14 18:55:58 -0500

    Manejo de pruebas para async y awaits instalando dependencias

---
+++ 03-counter-app-vite/jest.config.cjs
@@ -0,0 +1,5 @@
+/* Este doumento sirve para cosas donde se necesitan configuraciones previas */
+module.exports = {
+    //TODO: jsdom,
+    setupFiles: ['./jest.setup.js'] //Codigo que debe ser creado
+}
---
+++ 03-counter-app-vite/jest.setup.js
@@ -0,0 +1,4 @@
+//Codigo que se ejecutará al iniciar nuestras pruebas
+//Instalaremos un polify para que funcione correctamente
+//yarn add -D whatwg-fetch. Esta sólo es para ambiente de desarrollo
+import 'whatwg-fetch';
