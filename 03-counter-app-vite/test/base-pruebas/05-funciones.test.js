8da80f76476fe14cdd7159547a45ac31f1e7f41c
Yann <metalyanni@gmail.com>
2022-09-14 14:05:55 -0500

    Uso de toEqual y toStrictEqual para comparar objetos provenientes de funciones

--- 03-counter-app-vite/test/base-pruebas/05-funciones.test.js
+++ 03-counter-app-vite/test/base-pruebas/05-funciones.test.js
@@ -1,4 +1,4 @@
-import { getUser } from "../../src/base-pruebas/05-funciones";
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
 
 
 describe('Pruebas en 05-Funciones', () => {
@@ -15,5 +15,25 @@ describe('Pruebas en 05-Funciones', () => {
     expect( testUser ).toEqual( user);
     //Equivalente expect( testUser ).toStrictEqual( user);
   })
+
+  /**Ejercicio:
+   * Se espera un objeto donde venta también el nombre que le especificamos
+   * Si le mando el name a la función, esa parte del name también debe ser
+   * evaluada en el expect( testUser ).toEqual( user);
+   */
+  test('getUsuarioActivo debe de retornar un objeto',() =>{
+    //1
+    const name='Yanni';
+    //2
+    const user = getUsuarioActivo( name );
+    console.log(user);
+
+    //3
+    expect( user ).toStrictEqual({
+        uid:'ABC567',
+        username: name
+    })
+    
+  });
   
 })
