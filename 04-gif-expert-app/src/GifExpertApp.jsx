63706b7c80b81ba678f8487cb3a0d267b1c085ca
Yann <metalyanni@gmail.com>
2022-09-19 12:56:40 -0500

    Creando un hook basado en arreglos ý pintandolo en el HTML

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export const GifExpertApp = () =>{
+
+    //Espacio en memoria para visualizar estados, en este caso le mandamos un arreglo de categorias
+    const [categories, setcategories] = useState(['Dragon Ball','Smash bros']);
+
     return(
         <>
             {/* Titulo */}
@@ -9,6 +15,13 @@ export const GifExpertApp = () =>{
 
 
             {/* Listado de Gifs */}
+            <ol>
+                {/* Es forzoso mandar una llave */}
+                { categories.map( map =>{
+                    return <li key={category}>{category}</li>
+                }) }
+                <li></li>
+            </ol>
                 {/* Gif Item */}
         </>
     );
