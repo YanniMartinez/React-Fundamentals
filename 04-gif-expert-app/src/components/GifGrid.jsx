e6759664490c43f05591569828c06697914b59f5
Yann <metalyanni@gmail.com>
2022-09-19 17:44:40 -0500

    Uso del Hook useEffect para no tener que renderizar un componente muchas veces, sino 1 vez almenos para peticiones http como vemos en el ejemplo

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,28 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 //rafc
 export const GifGrid = ( {category }) => {
   
+  const [counter, setcounter] = useState(10);
+
+  //Uso del Hook useEffect: Es un Hook de React que permite lanzar efectos secundarios
+  //Un efecto secundario es algo que queremos que se ejecute despues de que suceda algo
+  //Aquí le indicaremos que cargue el HTTP unicamente cuando se cargue por primera vez
+  useEffect( () => {
+    getGifs(category);
+    
+  }, []);
+
 
   getGifs(category);
 
   return(
     <>
         <h3>{ category }</h3>
-        <p>Hola mundo</p>
+        <h5>Hola mundo</h5>
+        <button onClick={ () => setCounter(counter+1)}>+1</button>
     </>
   )
 }
