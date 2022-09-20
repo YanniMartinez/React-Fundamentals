a013bb3ec26e5566fc2884606da4dd9e3645ba83
Yann <metalyanni@gmail.com>
2022-09-19 22:21:57 -0500

    Creación de un archivo de Barril para simplificar imports

---
+++ 04-gif-expert-app/src/components/index.js
@@ -0,0 +1,7 @@
+/** Esto es un archivo de BARRIL
+ * Sirve para simplificar las lineas de importación
+ * que tenemos dentro de nuestros códigos
+ */
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
